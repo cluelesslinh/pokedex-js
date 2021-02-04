da01f9e02f802ad495198ce484c1da24151f645c
cluelesslinh <77943437+cluelesslinh@users.noreply.github.com>
2021-02-04 10:58:25 -0800

    update js
    added 3 pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello world');
-
-let favoriteFood = "yakitori";
-document.write(favoriteFood)
+var pokemonList = {
+  001: {
+    name: 'Bulbasaur',
+    height: 71.1,
+    type: ['Grass', 'Poison']
+  },
+  002: {
+    name: 'Ivysaur',
+    height: 99.1,
+    type: ['Grass', 'Poison']
+  },
+  003: {
+    name: 'Venusaur',
+    height: 201,
+    type: ['Grass', 'Poison']
+  },
+};
