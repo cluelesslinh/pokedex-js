aa47c086f449e8d4ec68a69d0cdfab9e1b18bf5b
cluelesslinh <77943437+cluelesslinh@users.noreply.github.com>
2021-02-09 11:04:24 -0800

    javascript basic 1.3
    created basic pokedex layout, included title.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
-var pokemonList = {
-  001: {
+let pokemonList = [
+  {
     name: 'Bulbasaur',
     height: 71.1,
     type: ['Grass', 'Poison']
   },
-  002: {
+  {
     name: 'Ivysaur',
     height: 99.1,
     type: ['Grass', 'Poison']
   },
-  003: {
+  {
     name: 'Venusaur',
     height: 201,
     type: ['Grass', 'Poison']
   },
-};
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 200) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') Wow, a huge Pokemon! <br>');
+  } else if (pokemonList[i].height < 100) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') Oh, a small Pokemon <br>');
+  } else {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + '<br>');
+  }
+}
