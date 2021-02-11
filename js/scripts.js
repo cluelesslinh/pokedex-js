e91d3b78bea8d83cf5cc0e35093140e0fcbeaddb
cluelesslinh <77943437+cluelesslinh@users.noreply.github.com>
2021-02-10 20:26:09 -0800

    task 1.5 part 1
    created forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -13,15 +13,9 @@ let pokemonList = [
     name: 'Venusaur',
     height: 201,
     type: ['Grass', 'Poison']
-  },
+  }
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 200) {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') Wow, a huge Pokemon! <br>');
-  } else if (pokemonList[i].height < 100) {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') Oh, a small Pokemon <br>');
-  } else {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + '<br>');
-  }
-}
+pokemonList.forEach(function(pokemon) {
+  document.write(pokemon.name + ' height is ' + pokemon.height + ' cm. <br>');
+});
