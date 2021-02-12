43e1c9280a2640db5e868dd8340f063e50ad8e54
cluelesslinh <77943437+cluelesslinh@users.noreply.github.com>
2021-02-11 17:32:23 -0800

    task 1.5 part 2
    created outside loop function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
+
+// IIFE pokemonRepository
+let pokemonRepository = (function () {
+
 let pokemonList = [
-  {
+{
     name: 'Bulbasaur',
     height: 71.1,
     type: ['Grass', 'Poison']
@@ -14,8 +18,37 @@ let pokemonList = [
     height: 201,
     type: ['Grass', 'Poison']
   }
-];
+  ];
 
-pokemonList.forEach(function(pokemon) {
-  document.write(pokemon.name + ' height is ' + pokemon.height + ' cm. <br>');
-});
+   return {
+      add: function(pokemon) {
+        pokemonList.push(pokemon);
+      },
+      getAll: function() {
+        return pokemonList;
+      }
+    };
+  })();
+
+// pokemon add and getAll function
+ console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Pikachu', height: 40.6, type: ['Electric'] });
+console.log(pokemonRepository.getAll());
+
+// function to show in browser
+  (function () {
+    pokemonRepository.getAll().forEach(function(pokemon) {
+        let pokemonName = pokemon.name
+        let pokemonHeight = pokemon.height
+
+        if(pokemonHeight > 200) {
+            document.write(pokemonName + ' (height : ' + pokemonHeight + ')' + ' - Wow, a huge Pokemon!');
+        } else if (pokemonHeight < 100) {
+          document.write(pokemonName + ' (height: ' + pokemonHeight + ') - Aw, a small Pokemon.');
+        }
+        else {
+            document.write(pokemonName + ' (height : ' + pokemonHeight + ')');
+        }
+            document.write('<br>', '<br>');
+        });
+})();
